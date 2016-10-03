4daf8231d014144903c42343aa7b08e398ec5e6d
Yauhen Kharuzhy <jekhor@gmail.com>
2016-10-03 17:07:47 +0300

    Add transparent versions of icons

--- split-icons.sh
+++ split-icons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsvg-convert -w 56 -o velo-icons-all-24.png velo-icons-all.svg
-rsvg-convert -w 112 -o velo-icons-all-48.png velo-icons-all.svg
+rsvg-convert -w 88 -o velo-icons-all-24.png velo-icons-all.svg
+rsvg-convert -w 176 -o velo-icons-all-48.png velo-icons-all.svg
 
 
 NAMES='icon-names.txt'
@@ -17,7 +17,7 @@ for mult in 1 2; do
 	SOURCE="velo-icons-all-$size.png"
 
 	x=0
-	for color in dark light; do
+	for color in dark light transparent; do
 		y=0
 		cat "$NAMES" |  while read name; do
 			convert -crop ${size}x${size}+$x+$y "$SOURCE" $DST/$name-$color-$size.png
