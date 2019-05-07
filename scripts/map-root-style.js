9744b4cc182ae9609f4b0932d8aaf8e9bd2ebaca
uniorunr <gagael23@live.com>
2019-05-07 13:10:53 +0300

    feature: use resize event listener to change width of the map on window resize

--- scripts/map-root-style.js
+++ scripts/map-root-style.js
@@ -7,3 +7,15 @@ maps.forEach((map) => {
   map.style.marginRight = 'calc(50% - 50vw)';
   map.style.zIndex = '0';
 });
+
+window.addEventListener('resize', function () {
+  const maps = Array.from(document.querySelectorAll('div[id^="eurovelo-wordpress-map-"]'));
+
+  maps.forEach((map) => {
+    map.style.width = 'calc(' + window.innerWidth + 'px - 12px)';
+    map.style.maxWidth = window.innerWidth + 'px';
+    map.style.marginLeft = 'calc(50% - 50vw + 8px)';
+    map.style.marginRight = 'calc(50% - 50vw)';
+    map.style.zIndex = '0';
+  });
+});
